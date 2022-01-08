6d22f2c6609b2e769853965585e472b87cb4721d
Arun Kumar <arun24822@gmail.com>
2022-01-08 21:15:11 +0530

    add imp libraries for database (sqlalchemy) and configure everything(link btw all these files)

--- application/__init__.py
+++ application/__init__.py
@@ -1,10 +1,28 @@
 #import FLask class into application
 
 from flask import Flask
+# from mongoengine.connection import connect
+# from config import Config
+# from flask_mongoengine import MongoEngine
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+from flask_jwt_extended import JWTManager, jwt_required, create_access_token
+import os
+
 
 #intantiate an object 
 
 app = Flask(__name__)#identify current application /module that is passed through flask.\
-
+# app.config.from_object(Config)
+# db = MongoEngine()
+# db.init_app(app)
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'coffer.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app) #db is the sqlalchemy object.
+marsh = Marshmallow(app)
+jwt = JWTManager(app)
 
 from application import routes #seperation of concerns
+from application import api
+
--- application/routes.py
+++ application/routes.py
@@ -15,3 +15,24 @@ def login_():
 @app.route("/check-your-health-advanced")
 def check_your_health_advanced():
     return render_template('/includes/check_your_health_advanced.html')
+
+
+@app.route("/check-your-health-common")
+def check_your_health_common():
+    return render_template('/includes/check-your-health-common.html')
+
+
+# class User(db.Document):
+#     user_id     =   db.IntField(unique=True)
+#     first_name  =   db.StringField(max_length = 50)
+#     last_name   =   db.StringField(max_length = 50)
+#     email       =   db.StringField(max_length = 30)
+#     password    =   db.StringField(max_length = 30)
+
+
+# @app.route("/user")
+# def user():
+#     User(user_id = 1, first_name = 'Arun', last_name = 'Kumar', email = 'arun@gmail.com', password = '123456').save()
+#     User(user_id = 2, first_name = 'Shreya', last_name = 'Swain', email = 'shreya@gmail.com', password = '$123456').save()
+#     users = User.objects.all()
+#     return render_template('/includes/user.html', users=users)
